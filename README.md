09e247786e4cc0761844c6ea163f73b686ecb966
Josiah Valdez <46499348+ultramolecular@users.noreply.github.com>
2025-01-06 17:30:30 -0600

    Stock Criteria Tracking (and more) Feature) (#5)
    Implemented stock criteria met time, price, max price + time + volume + pct chg, EOD stats summary, bot wait until market open

--- README.md
+++ README.md
@@ -39,12 +39,12 @@ Run `python3 stockBot.py` in a terminal, and it will prompt you to enter:
 
 Once you entered your configuration, it will immediately begin working, and display the top 5 gainers with a current time stamp, their price, and absolute percent changes since the moment you initiated the bot.
 In addition to the absolute percent changes, it will include the past 1 minute, 5 minute, 10 minute, and 20 minutes percent changes.
-It will send you notifications when your criteria has been met (on the absolute scale).
+It will send you notifications when your criteria has been met (on the absolute scale), and once a stock has met your criteria and is in the top 5, it will provide an additional line describing the time and price it met criteria, along with the time, price, volume, and percent increase from the price at met criteria. This is useful additional information.
 
-Aside sending a notification, one of the features of this tool is a parrot notification mechanism, e.g if $GME grew the `y%` you wanted it to notify you, it will send a notification each `y'%` that it grows after.
+Aside sending a notification, one of the features of this tool is a parrot notification mechanism, e.g. if $GME grew the `y%` you wanted it to notify you, it will send a notification each `y'%` that it grows after.
 
 > [!NOTE]
 > - Start the program at 9:30 ET for the full range of coverage and best analytics.
-> - A premium membership account with TradingView is preferred, as it makes the best of it having real-time data, but a free membership will work too.
+> - A premium membership account with TradingView is preferred, as it makes the best of it having real-time data, but a free membership or no membership will work too.
 > - You cannot initiate the bot multiple instances with the same account credentials, unless you have the premium account that allows you to do so.
 > - This is a constant work in progress, bugs should be brought in up issues!
--- stock.py
+++ stock.py
@@ -8,15 +8,7 @@
 #-------------------------------------------------------------------------------------------------------#
 
 class Stock:
-    #-----------------------------------------------------------------------------#
-    # Creates a new stock and sets ticker, current price, original price,         #
-    # and percent change.                                                         #
-    # Args:                                                                       #
-    #     ticker (str): is the name of the stock, or ticker.                      #
-    #     price (float): price of the stock at the given moment.                  #
-    #     currTime (datetime): the time this stock was introduced to the gainers. #
-    #-----------------------------------------------------------------------------#
-    def __init__(self, ticker, price, currTime):
+    def __init__(self, ticker, price, vol, currTime):
         self.TICKER = ticker
         self.price = price
         self.basePrice = price
@@ -31,119 +23,130 @@ class Stock:
         self.fiveMinPctChg = 0.0
         self.tenMinPctChg = 0.0
         self.twentyMinPctChg = 0.0
+        self.critTime = None
+        self.critPrice = None
+        self.maxPrice = price
+        self.timeMaxPrice = None
+        self.volumeAtMaxPrice = vol
 
-    #--------------------------------------#
-    # Retuns the name/ticker of the stock. #
-    #--------------------------------------#
     def getTicker(self):
         return self.TICKER
 
-    #---------------------------------------------------------------#
-    # Calculate the stock's new absolute pct chg give the new price #
-    # (potentially) of the stock.                                   #
-    # Args:                                                         #
-    #     price (float): current price of the stock.                #
-    # Returns:                                                      #
-    #     (float): the new absolute pct chg of stock.               #
-    #-------------------------------------------------------------- #
     def getNewAbs(self, price):
+        """Calculate the new absolute percent change relative to OG_PRICE."""
         return ((price - self.OG_PRICE) / self.OG_PRICE) * 100
 
-    #--------------------------------------------------------------#
-    # Sets the new relative pct chg to a new updated percentage if #
-    # it has already met and passed the user criteria.             #
-    # Args:                                                        #
-    #     price (float): current price of the stock.               #
-    #--------------------------------------------------------------#
     def setNewAfter(self, newPrice):
+        """Sets the relative % change if user criteria is already met."""
         self.pctChgAfter = ((newPrice - self.basePrice) / self.basePrice) * 100 if self.metCrit else 0
 
-    #--------------------------------------------#
-    # Returns the relative pct chg of the stock. #
-    #--------------------------------------------#
     def getAfter(self):
         return self.pctChgAfter
 
-    #-----------------------------------------------------#
-    # Returns the absolute pct chg of the stock the user. #
-    #-----------------------------------------------------#
     def getAbs(self):
         return self.absPctChg
-    
-    #---------------------------------------------------------------#
-    # Sets the absolute pct chg of the stock to a given percentage. #
-    # Args:                                                         #
-    #     newAbs (float): the new absolute pct chg of stock.        #
-    #---------------------------------------------------------------#
+
     def setAbs(self, newAbs):
         self.absPctChg = newAbs
 
-    #-------------------------------------------------------------#
-    # Returns whether the stock has met the criteria of the user. #
-    #-------------------------------------------------------------#
     def hasMetCrit(self):
         return self.metCrit
 
-    #--------------------------------------------------#
-    # Flags that the stock has met the user criteria.  #
-    #--------------------------------------------------#
     def didMeetCrit(self):
+        """
+        Flags that the stock has met user criteria, and records the time/price
+        if not already set.
+        """
         self.metCrit = True
+        # Only record the first time we met criteria
+        if self.critTime is None:
+            from datetime import datetime
+            self.critTime = datetime.now()
+            self.critPrice = self.price
+            # Initialize max tracking to current
+            self.maxPrice = self.price
+            self.timeMaxPrice = datetime.now()
 
-    #---------------------------------------------------#
-    # Sets the price of the stock to a given new price. #
-    # Args:                                             #
-    #     newPrice (float): the new price of the stock. #
-    #---------------------------------------------------#
     def setPrice(self, newPrice):
         self.price = newPrice
 
-    #----------------------------------------------------#
-    # Sets the base price of the stock to a given price. #
-    # Args:                                              #
-    #     newPrice (float): the new price of the stock.  #
-    #----------------------------------------------------#
     def setBasePrice(self, newPrice):
         self.basePrice = newPrice
 
-    #------------------------------------------------------------------#
-    # Checks for each interval and updates their pct chgs accordingly. #
-    # Args:                                                            #
-    #     price (float): current price of the stock.                   #
-    #     currTime (datetime): current time used to measure age.       #
-    #------------------------------------------------------------------#     
-    def updateIntervals(self, price, currTime):
-        # Set age of stock given the current time and the time it entered.
+    def setMaxPrice(self, newMaxPrice):
+        self.maxPrice = newMaxPrice
+
+    def setTimeMaxPrice(self, newTimeMaxPrice):
+        self.timeMaxPrice = newTimeMaxPrice
+
+    def setVolAtMaxPrice(self, newVolAtMax):
+        self.volumeAtMaxPrice = newVolAtMax
+
+    def getMaxPrice(self):
+        return self.maxPrice
+
+    def getTimeMaxPrice(self):
+        return self.timeMaxPrice
+
+    def getVolAtMaxPrice(self):
+        return self.volumeAtMaxPrice
+
+    def getCritTime(self):
+        return self.critTime
+
+    def getCritPrice(self):
+        return self.critPrice
+
+    def getPeakChange(self):
+        """
+        Compute how much the maxPrice is, in percent change, relative to the
+        price when stock hit criteria (critPrice).
+        """
+        return ((self.maxPrice - self.critPrice) / self.critPrice) * 100
+
+    def updateIntervals(self, price, vol, currTime):
+        """
+        Update the 1m, 5m, 10m, 20m intervals, age of the stock, as well as criteria
+        stats once stock has met criteria. Called from the main loop in stockBot.py.
+        """
         self.age = (currTime - self.TIME_ENTERED).seconds // 60
-        # Add the current price to the queue of past prices.
         self.pastPrices.append(price)
         qLen = len(self.pastPrices)
+
+        # 1-minute interval
         if self.age >= 1:
-            # This is just error prevention, sometimes it doesn't actually have enough prices held.
             i = qLen - 2 if qLen - 2 >= 0 else 0
             self.oneMinPctChg = ((price - self.pastPrices[i]) / self.pastPrices[i]) * 100
+        
+        # 5-minute interval
         if self.age >= 5:
             i = qLen - 6 if qLen - 6 >= 0 else 0
             self.fiveMinPctChg = ((price - self.pastPrices[i]) / self.pastPrices[i]) * 100
+        
+        # 10-minute interval
         if self.age >= 10:
             i = qLen - 11 if qLen - 11 >= 0 else 0
             self.tenMinPctChg = ((price - self.pastPrices[i]) / self.pastPrices[i]) * 100
+        
+        # 20-minute interval
         if self.age >= 20:
             i = qLen - 21 if qLen - 21 >= 0 else 0
             self.twentyMinPctChg = ((price - self.pastPrices[i]) / self.pastPrices[i]) * 100
-            # Make sure that once we are at the 20 price limit, to pop the first price.
+            # Keep queue size limited
             self.pastPrices.pop(0)
-        
-    #-----------------------------------------------------------------------#
-    # Stringifies a stock to its ticker, price, absolute pct chg, along     #
-    # with its respective past 1-minute, 5-minute, 10-minute, and 20-minute #
-    # pct chg.                                                              #
-    # Returns:                                                              #
-    #     (str): string that represents a stock object.                     #
-    #-----------------------------------------------------------------------#
+
+        # If we've already met criteria, see if price is a new maximum
+        if self.metCrit and price > self.maxPrice:
+            self.maxPrice = price
+            self.timeMaxPrice = currTime 
+            self.volumeAtMaxPrice = vol
+
     def __str__(self):
+        """
+        Print the stock's main line: TICKER, Price, Abs % change, and interval changes.
+        We'll rely on the main script to optionally print the second line if hasMetCrit().
+        """
         s = f"{self.TICKER}\t${self.price}\tAbs Pct-Chg: {self.absPctChg:.2f}%"
-        # Check for age to add appropriate interval pct chgs.
         if self.age >= 1:
             s += f"\t1m Pct-Chg: {self.oneMinPctChg:.2f}%"
         if self.age >= 5:
